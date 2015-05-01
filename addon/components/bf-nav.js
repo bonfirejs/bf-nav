09a6bcfb83224a1251d4601969a8562e7b273b09
Alex Houchens <gregoryx.houchens@intel.com>
2015-05-01 09:15:49 -0700

    Add support for nested routes
    Closes #1

--- addon/components/bf-nav.js
+++ addon/components/bf-nav.js
@@ -8,12 +8,38 @@ export default Ember.Component.extend({
     routes = routes.filter(function(item) {
       return !item.endsWith('index') && !item.endsWith('loading') && !item.endsWith('error') && item !== 'application';
     }).map(function(item) {
+      var name = item.split('.');
+      name = name[name.length - 1];
       return {
         routeName: item,
-        name: item.capitalize()
+        name: name.capitalize()
       };
     }).sortBy('name');
-    routes.unshiftObject({ routeName: 'index', name: 'Home' });
-    return routes;
+    // Add nested
+    routes.forEach( function(route, index, enumerable) {
+      if (route.routeName.indexOf('.') !== -1) { // nested
+        var parentRoute = route.routeName.split('.');
+        parentRoute.pop();
+        parentRoute = parentRoute.join('.');
+        parentRoute = routes.findBy('routeName', parentRoute);
+        if (parentRoute.children == undefined) {
+          parentRoute.children = [];
+        }
+        parentRoute.children.pushObject(route);
+      }
+    });
+    // Remove redundant objects
+    var revisedRoutes = [];
+    routes.forEach( function(route) {
+      if (route.routeName.indexOf('.') == -1) {
+        revisedRoutes.pushObject(route);
+      }
+    });
+    revisedRoutes.unshiftObject({ routeName: 'index', name: 'Home' });
+    return revisedRoutes;
   }.property('router.router.recognizer.names')
+});
+
+Ember.LinkView.reopen({
+  attributeBindings: ['data-toggle', 'role', 'aria-expanded']
 });
